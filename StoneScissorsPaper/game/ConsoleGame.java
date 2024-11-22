8c39394f3d79e72cecabeaf834d066e30ca47a3a
A27nastasi <anastasiia.mykhailenki@stud.onu.edu.ua>
2024-11-22 19:49:59 +0200

    Add changes

--- StoneScissorsPaper/game/ConsoleGame.java
+++ StoneScissorsPaper/game/ConsoleGame.java
@@ -2,27 +2,59 @@ package StoneScissorsPaper.game;
 
 import StoneScissorsPaper.Choice;
 import StoneScissorsPaper.player.RegularPlayer;
-import StoneScissorsPaper.player.Player;
 import StoneScissorsPaper.validate.Validate;
 
 public class ConsoleGame implements Game {
-    private RegularPlayer player1;
-    private RegularPlayer player2;
-    private Validate validate;
+    private final RegularPlayer player1;
+    private final RegularPlayer player2;
+    private final Validate validate;
 
-    public ConsoleGame(Player player1, Player player2, Validate validate) {
-        this.player1 = (RegularPlayer) player1;
-        this.player2 = (RegularPlayer) player2;
+    private int player1Wins = 0;
+    private int player2Wins = 0;
+    private int totalRounds = 0;
+
+    public ConsoleGame(RegularPlayer player1, RegularPlayer player2, Validate validate) {
+        this.player1 = player1;
+        this.player2 = player2;
         this.validate = validate;
     }
 
     @Override
     public void start() {
         System.out.println("Welcome to the game: ROCK, PAPER, SCISSORS");
+        System.out.println("First to 3 wins will be the champion!");
 
-        Choice choice1 = player1.makeChoice();
-        Choice choice2 = player2.makeChoice();
+        while (player1Wins < 3 && player2Wins < 3) {
+            totalRounds++;
+            System.out.println("\nRound " + totalRounds + ":");
 
-        validate.determineWinner(player1, choice1, player2, choice2);
+            Choice choice1 = player1.makeChoice();
+            Choice choice2 = player2.makeChoice();
+
+            System.out.println(player1.getName() + " chose " + choice1);
+            System.out.println(player2.getName() + " chose " + choice2);
+
+            if (choice1 == choice2) {
+                System.out.println("It's a draw!");
+            } else if (choice1.beats(choice2)) {
+                player1Wins++;
+                System.out.println(player1.getName() + " wins this round!");
+            } else {
+                player2Wins++;
+                System.out.println(player2.getName() + " wins this round!");
+            }
+
+            System.out.println("Current score: " + player1.getName() + " " + player1Wins + " - " + player2Wins + " " + player2.getName());
+        }
+
+        // Output the game results
+        System.out.println("\nGame Over!");
+        if (player1Wins == 3) {
+            System.out.println(player1.getName() + " is the champion!");
+        } else {
+            System.out.println(player2.getName() + " is the champion!");
+        }
+        System.out.println("Total rounds played: " + totalRounds);
+        System.out.println("Final score: " + player1.getName() + " " + player1Wins + " - " + player2Wins + " " + player2.getName());
     }
 }
